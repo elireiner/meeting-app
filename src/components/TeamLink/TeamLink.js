b3294a4861325b6bd2ffa4608f7025271ed427f1
Eli Reiners <goelyukim@gmail.com>
2020-09-27 10:18:42 -0400

    remove logging

--- src/components/TeamLink/TeamLink.js
+++ src/components/TeamLink/TeamLink.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default class TeamLink extends React.Component {
     render() {
-        console.log(this.props)
         const newTo = {
             pathname: `team/${this.props._id}`,
             team: this.props
