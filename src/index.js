5e16daea6dc49a2a644d00edb8836611ed0228b2
Eli Reiners <goelyukim@gmail.com>
2020-10-06 11:13:31 -0400

    Add context

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import { TeamListProvider } from './contexts/TeamListContext'
+import { TeamProvider } from './contexts/TeamContext'
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <TeamListProvider>
+            <TeamProvider>
+                <App />
+            </TeamProvider>
+        </TeamListProvider>
     </BrowserRouter>, document.getElementById('root'));
