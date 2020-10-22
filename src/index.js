062298f1ee95428a93c4aafe85cb2e2a7769cc09
Eli Reiners <goelyukim@gmail.com>
2020-10-22 08:38:12 -0400

    Update to new teams page route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import { TeamListProvider } from './contexts/TeamListContext'
+import { TeamListProvider } from './contexts/TeamsContext'
 import './index.css';
 
 ReactDOM.render(
