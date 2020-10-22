d45e5bdd26906123b4b3025fa6d0bb412ebb926d
Eli Reiners <goelyukim@gmail.com>
2020-10-21 21:19:12 -0400

    Update after removing file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { TeamListProvider } from './contexts/TeamListContext'
-import { TeamProvider } from './contexts/TeamContext'
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
         <TeamListProvider>
-            <TeamProvider>
-                <App />
-            </TeamProvider>
+            <App />
         </TeamListProvider>
     </BrowserRouter>, document.getElementById('root'));
