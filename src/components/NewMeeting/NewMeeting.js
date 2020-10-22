b02978d634ce8afb9bac2fdbf100e71cb57320ba
Eli Reiners <goelyukim@gmail.com>
2020-10-21 20:57:23 -0400

    Add prop for allTeams

--- src/components/NewMeeting/NewMeeting.js
+++ src/components/NewMeeting/NewMeeting.js
@@ -5,12 +5,21 @@ import './NewMeeting.css';
 export default class NewMeeting extends React.Component {
 
     render() {
+        const usersTeams = { 
+            pathname: `/teams`, 
+            allTeams: false
+          };
+
+        const allTeams = { 
+            pathname: `/teams`, 
+            allTeams: true
+          };
         return (
             <>
                 <nav className="newMeetingNav">Meeting app</nav>
                 <section className="newMeetingBody">
-                    <Link to='/teams'>Choose from your teams</Link>
-                    <Link to='/teams'>Join a new team</Link>
+                    <Link to={usersTeams}>Choose from your teams</Link>
+                    <Link to={allTeams}>Join a new team</Link>
                 </section>
             </>
         )
