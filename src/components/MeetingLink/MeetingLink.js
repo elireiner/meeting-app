40a8803a822f14d2c2ff319f1c4a6dc26f580b39
Eli Reiners <goelyukim@gmail.com>
2020-11-03 15:48:08 -0500

    Add path dynamically

--- src/components/MeetingLink/MeetingLink.js
+++ src/components/MeetingLink/MeetingLink.js
@@ -3,13 +3,30 @@ import { Link } from 'react-router-dom';
 
 export default class MeetingLink extends React.Component {
     render() {
-        const path = { 
-            pathname: `meeting/${this.props._id}`, 
-            meeting: this.props
-          };
+        let path;
+
+        if (this.props.render) {
+            console.log(this.props)
+            path = (this.props.render.onlyRecurring && this.props.render.onlyTrends) ?
+                {
+                    pathname: `recurring-meeting/${this.props.meeting.meeting_id}/trends`,
+                    state: { meeting: this.props }
+                } : {
+                    pathname: `meeting/${this.props.meeting.meeting_id}`,
+                    state: { meeting: this.props }
+                }
+        }
+        else {
+            path = {
+                pathname: `meeting/${this.props.meeting._id}`,
+                state: { meeting: this.props }
+            }
+        }
+
+        console.log(path)
         return (
             <>
-                <Link to={path}>{this.props.name}</Link>
+                <li> <Link to={path}>{this.props.meeting.meeting_name}</Link> </li>
             </>
         )
     }
