c069f779671d10b8267823fef02bafbdb8661225
Eli Reiners <goelyukim@gmail.com>
2020-11-03 15:46:30 -0500

    Add contexts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import { TeamsProvider } from './contexts/TeamsContext'
+import { TeamsProvider } from './contexts/TeamsContext';
+import { MeetingsProvider } from './contexts/MeetingsContext';
+import { AssessmentsProvider } from './contexts/AssessmentsContext';
 import './index.css';
 
 ReactDOM.render(
     <BrowserRouter>
         <TeamsProvider>
-            <App />
+            <MeetingsProvider>
+                <AssessmentsProvider>
+                    <App />
+                </AssessmentsProvider>
+            </MeetingsProvider>
         </TeamsProvider>
     </BrowserRouter>, document.getElementById('root'));
