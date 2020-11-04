7fb31ebc4f44e14375f889bf75a9e0c8d1ec3d34
Eli Reiners <goelyukim@gmail.com>
2020-11-04 16:15:06 -0500

    Pass only meeting

--- src/components/MeetingLink/MeetingLink.js
+++ src/components/MeetingLink/MeetingLink.js
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom';
 export default class MeetingLink extends React.Component {
     render() {
         let path;
-
         if (this.props.render) {
-            console.log(this.props)
             path = (this.props.render.onlyRecurring && this.props.render.onlyTrends) ?
                 {
                     pathname: `recurring-meeting/${this.props.meeting.meeting_id}/trends`,
-                    state: { meeting: this.props }
+                    state: { meeting: this.props.meeting }
                 } : {
                     pathname: `meeting/${this.props.meeting.meeting_id}`,
                     state: { meeting: this.props }
@@ -23,7 +21,6 @@ export default class MeetingLink extends React.Component {
             }
         }
 
-        console.log(path)
         return (
             <>
                 <li> <Link to={path}>{this.props.meeting.meeting_name}</Link> </li>
