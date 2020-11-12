af8b4b824294cc563ee75aab9b0516e5e216abe8
Eli Reiners <goelyukim@gmail.com>
2020-11-12 10:47:30 -0500

    Add file type

--- src/components/MeetingLink/MeetingLink.js
+++ src/components/MeetingLink/MeetingLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../MainLinks';
+import '../MainLinks.css';
 
 export default class MeetingLink extends React.Component {
     render() {
--- src/components/TeamLink/TeamLink.js
+++ src/components/TeamLink/TeamLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../MainLinks';
+import '../MainLinks.css';
 
 export default class TeamLink extends React.Component {
     render() {
