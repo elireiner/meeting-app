761defe38e3fe1112fb3a35dea9a94eb2ff32358
Eli Reiners <goelyukim@gmail.com>
2020-11-19 13:38:19 -0500

    Change to checkbox Add fontawesome

--- src/components/UserLink/UserLink.js
+++ src/components/UserLink/UserLink.js
@@ -1,5 +1,8 @@
 import React from 'react';
 //import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
 import '../MainLinks.css';
 
 export default class UserLink extends React.Component {
@@ -10,29 +13,32 @@ export default class UserLink extends React.Component {
         }
     };
     handleClick = async () => {
-        const selectedState = !this.state.selected 
+        const selectedState = !this.state.selected
         await this.setState({
             selected: selectedState
         })
-        
-}
-render() {
-    /*let path = {
-        pathname: `user/${this.props.user._id}`,
-        state: { user: this.props }
-    }*/
 
-    const buttonText = (this.state.selected) ? 'unselect' : 'select'
-    return (
-        <>
-            {// <Link className='mainLink' to={path}>{this.props.user.first_name}</Link>
-            }
-            <div className="mainLinkDiv">
-                <p >{this.props.user.first_name} {this.props.user.last_name}</p>
-                <button onClick={this.handleClick}>{buttonText}</button>
-            </div>
+    }
+    render() {
+        /*let path = {
+            pathname: `user/${this.props.user._id}`,
+            state: { user: this.props }
+        }*/
 
-        </>
-    )
-}
+        //const buttonText = (this.state.selected) ? 'unselect' : 'select'
+        return (
+            <>
+                {// <Link className='mainLink' to={path}>{this.props.user.first_name}</Link>
+                }
+                <div className="mainLinkDiv">
+                    <p>{this.props.user.first_name} {this.props.user.last_name}</p>
+                    { 
+                    // <button onClick={this.handleClick}>{buttonText}</button> <FontAwesomeIcon icon={faCheckSquare} />
+                    }
+                    <input className="selectCheckBox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                </div>
+
+            </>
+        )
+    }
 }
