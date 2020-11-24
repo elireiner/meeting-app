2108d0acea222ee4322be99c15e3e93d3ae028f7
Eli Reiners <goelyukim@gmail.com>
2020-11-24 11:32:14 -0500

    Remove unused code

--- src/components/UserLink/UserLink.js
+++ src/components/UserLink/UserLink.js
@@ -1,50 +1,22 @@
 import React from 'react';
 //import { Link } from 'react-router-dom';
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
-
 import '../MainLinks.css';
 
 export default class UserLink extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: false
-        }
-    };
-    handleClick = async () => {
-        const selectedState = !this.state.selected
-        await this.setState({
-            selected: selectedState
-        })
-
-    }
     render() {
-        /*let path = {
-            pathname: `user/${this.props.user._id}`,
-            state: { user: this.props }
-        }*/
         const userId = this.props.user.user_id
-        //const buttonText = (this.state.selected) ? 'unselect' : 'select'
         return (
             <>
-                {// <Link className='mainLink' to={path}>{this.props.user.first_name}</Link>
-                }
-                <div className="mainLinkDiv">
+                <div className="blockDiv">
                     <p>{this.props.user.first_name} {this.props.user.last_name}</p>
-                    {
-                        // <button onClick={this.handleClick}>{buttonText}</button> <FontAwesomeIcon icon={faCheckSquare} />
-                    }
                     <input
                         className="selectCheckBox"
                         type="checkbox"
                         key={userId}
-                        name="vehicle1"
-                        value="Bike"
-                        onChange={(e) => this.props.handelSelection(e, userId)}
+                        name="checkBox"
+                        value={userId}
                     />
                 </div>
-
             </>
         )
     }
